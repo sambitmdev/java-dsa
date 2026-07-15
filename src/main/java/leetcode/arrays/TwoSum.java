e4598a2564ae4ed39d51f48d33a8e86ac3939e93
sambit-m95 <dev.sambit95@gmail.com>
2026-07-15 22:56:34 +0530

    A1: Two Sum - brute O(n2) SC O(1) and HashMap O(n) SC O(n) solutions

--- src/main/java/leetcode/arrays/TwoSum.java
+++ src/main/java/leetcode/arrays/TwoSum.java
@@ -1,9 +1,10 @@
 package leetcode.arrays;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
-public class TwoSum1 {
-    public int[] twoSum(int[] nums, int target){
+public class TwoSum {
+    public int[] twoSumBrute(int[] nums, int target){
 
         for(int i = 0 ; i < nums.length-1; i++){
             int temp = target - nums[i];
@@ -17,10 +18,24 @@ public class TwoSum1 {
     }
     //TC = O(n2)
     //SC = O(1)
+
+    public int[] twoSumOptimised(int[] nums, int target){
+        HashMap<Integer, Integer> indexMap = new HashMap<>();
+        for(int i = 0 ; i < nums.length; i++){
+            int temp = target - nums[i];
+            if (indexMap.containsKey(temp)){
+                return new int[]{i, indexMap.get(temp)};
+            }
+            indexMap.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No Solution Found");
+    }
+    //TC = O(n)
+    //SC = O(n)
     public static void main(String[] args) {
-        TwoSum1 twoSum1 = new TwoSum1();
+        TwoSum twoSum = new TwoSum();
         int[] a = {2,7,11,15};
-        int[] ints = twoSum1.twoSum(a, 18);
+        int[] ints = twoSum.twoSumOptimised(a, 18);
         System.out.println(Arrays.toString(ints));
     }
 }
